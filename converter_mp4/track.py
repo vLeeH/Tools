8b2ec43e90acca2017d87f68549e4242a3248e7b
Lee <vitorleehan123@gmail.com>
2021-01-17 16:19:03 -0300

    Update track.py

--- converter_mp4/track.py
+++ converter_mp4/track.py
@@ -4,14 +4,14 @@ try:
 except ImportError:
     os.system("pip install pyrastreio")
 
-print("!!!ATENÇÃO ESTE PROGRAMA FUNCIONA APENAS PARA RASTREIO DE ENCOMENDAS DAS TRANSPORTADORAS [SEDEX, CORREIOS]!!!")
+print("!!!ATTENTION THIS PROGRAM WORKS ONLY FOR TRACKING ORDERS OF CARRIERS [SEDEX, MAILS]!!!")
 while True:
-    intt = str(input("CODIGO DE RASTREIO: "))
+    intt = str(input("TRACKING CODE: "))
     res = correios(intt)
     try:
         print(f'{res[0]["data"]} | {res[0]["hora"]} | {res[0]["mensagem"]}')
     except:
-        print("o codigo não funcionou tente novamente!")
+        print("The code did not work please try again!")
 
     for i in range(1, 6):
         try:
