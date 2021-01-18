86cfc7335baed478a19f2c809f6b24e5ab01fa97
Lee <vitorleehan123@gmail.com>
2021-01-18 16:04:21 -0300

    Update track.py

--- Track/track.py
+++ Track/track.py
@@ -1,21 +1,31 @@
+# github.com/vLeeH - 2021
+
 import os
 try:
     from pyrastreio import correios
 except ImportError:
     os.system("pip install pyrastreio")
 
-print("!!!ATTENTION THIS PROGRAM WORKS ONLY FOR TRACKING ORDERS OF CARRIERS [SEDEX, MAILS]!!!")
+print('-='*25)
+print("THIS PROGRAM WORKS ONLY FOR TRACKING ORDERS OF CARRIERS [SEDEX, MAILS]!")
+
 while True:
-    intt = str(input("TRACKING CODE: "))
-    res = correios(intt)
+    # Get the code of the order
+    code = str(input("TRACKING CODE: "))
+    resu = correios(code)
     try:
-        print(f'{res[0]["data"]} | {res[0]["hora"]} | {res[0]["mensagem"]}')
-    except:
-        print("The code did not work please try again!")
+        print(f'{resu[0]["data"]} | {resu[0]["hora"]} | {resu[0]["mensagem"]}')
+    except Exception as e:
+        print(f"[ERROR]The code did not work please try again! \n {e}")
 
-    for i in range(1, 6):
-        try:
-            print(f'{res[i]["data"]} | {res[1]["hora"]} | {res[i]["mensagem"]}')
-        except:
-            pass
+    try: 
+        for i in range(1, 6):
+            try:
+                print(f'{resu[i]["data"]} | {resu[1]["hora"]} | {resu[i]["mensagem"]}')
+            except:
+                pass
+    
+    except Exception as e: 
+        print(f'[ERROR] {e}')
 
+# https://github.com/vLeeH/ToolsPy/edit/main/Track/track.py
